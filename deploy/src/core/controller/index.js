b881a9d382d5835214480a9150d323dc8a8bba8a
carlos quintero <info.fxstudios@gmail.com>
2022-07-14 10:25:15 -0500

    update

--- deploy/src/core/controller/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.controllers = void 0;
-const test_controller_1 = require("./test.controller");
-const test = new test_controller_1.TestController();
-exports.controllers = {
-    test
-};
